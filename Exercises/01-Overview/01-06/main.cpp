167d25e7a840bfee3518818ab7b9ada3b180fe31
Amir Hesham <amirhesham65@gmail.com>
2022-09-03 08:29:40 +0200

    Solve chapter 01 - exercise 06

---
+++ Exercises/01-Overview/01-06/main.cpp
@@ -0,0 +1,45 @@
+// main.cpp
+//
+// 
+// Write a program that reads in a list of integers from the user until the user enters the 
+// value 0 as a sentinel. When the sentinel appears, your program should display the largest 
+// value in the list (and the second largest)
+//
+// --------------------------------------------------------------------------
+// Attribution: "Programming Abstractions in C++" by Eric Roberts
+// Chapter 1, Exercise 6
+// Stanford University, Autumn Quarter 2012
+// http://web.stanford.edu/class/archive/cs/cs106b/cs106b.1136/materials/CS106BX-Reader.pdf
+// --------------------------------------------------------------------------
+//
+// Created by Amir Hesham
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+    int max = 0;
+    int secondMax = 0;
+
+    const int SENTINEL = 0;
+
+    cout << "This program finds the largest integer in a list." << endl;
+    cout << "Enter 0 to signal the end of the list" << endl;
+
+    while (true)
+    {
+        cout << "\t? ";
+        cin >> num;
+        if (num == SENTINEL) break;
+        if (num > max) {
+            int temp = max;
+            max = num;
+            secondMax = temp;
+        }
+    }
+
+    cout << "The largest number is: " << max << endl;
+    cout << "The second largest number is: " << secondMax << endl;
+}
