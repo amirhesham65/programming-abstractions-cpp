2fa07bfc194206b3b90613cfe0bd4360b86fd0da
Amir Hesham <amirhesham65@gmail.com>
2022-09-05 23:54:26 +0200

    Add starter-project template

---
+++ starter-project/src/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//#include "console.h"
+#include <random.h>
+//#include <error.h>
+using namespace std;
+
+int main() {
+    cout << randomInteger(1, 5) << endl;
+
+    return 0;
+}
